e41a57da89bd520ee0e33392f7bda38372d9178f
Associate <Associate@roifmrwindt>
2023-10-11 10:34:09 +0530

    added routing for shopping cart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomepageComponent} from "./components/homepage/homepage.component";
 import {ProductInfoComponent} from "./components/product-info/product-info.component";
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent, pathMatch: 'full' },
   { path: 'product', component: ProductInfoComponent },
+  { path: 'Cart', component: ShoppingCartComponent }
 ];
 
 @NgModule({
